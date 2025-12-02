03571e915207ec5a05b994f0406e0910c7a290b6
Matthew Hopson <mhopson@hotmail.co.uk>
2025-12-02 18:11:34 +0000

    Day 2 part 2

--- day2.go
+++ day2.go
@@ -16,30 +16,30 @@ func day_two() {
 		return
 	}
 
-	pattern := `^(.+)\1$`
-	re := regexp2.MustCompile(pattern, 0)
-
-	text := "123123"
-	match, _ := re.MatchString(text)
-
-	fmt.Println(match)
+	re_p1 := regexp2.MustCompile(`^(.+)\1$`, 0)
+	re_p2 := regexp2.MustCompile(`^(.+)\1+$`, 0)
 
 	product_id_ranges := strings.Split(string(content), ",")
-	var total = 0
+	var total_part1 = 0
+	var total_part2 = 0
 
-	for index, product_id_range := range product_id_ranges {
+	for _, product_id_range := range product_id_ranges {
 		product_ids := strings.Split(string(product_id_range), "-")
 
 		begin, _ := strconv.Atoi(product_ids[0])
 		end, _ := strconv.Atoi(product_ids[1])
 
 		for i := begin; i <= end; i++ {
-			match, _ := re.MatchString(strconv.Itoa(i))
+			match, _ := re_p1.MatchString(strconv.Itoa(i))
 			if match {
-				fmt.Printf("%d -> %d\n", index, i)
-				total += i
+				total_part1 += i
+			}
+
+			match2, _ := re_p2.MatchString(strconv.Itoa(i))
+			if match2 {
+				total_part2 += i
 			}
 		}
 	}
-	fmt.Printf("Total: %d ", total)
+	fmt.Printf("Part1: %d - Part2: %d ", total_part1, total_part2)
 }
