26c5eb7c77cda82417c8c9eab3526d63e92c0be8
Matthew Hopson <mhopson@hotmail.co.uk>
2025-12-04 18:42:02 +0000

    Day 4 part 2

--- day4.go
+++ day4.go
@@ -6,24 +6,19 @@ import (
 	"strings"
 )
 
-func day_four() {
-	content, err := os.ReadFile("input/day4.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+type xy struct {
+	x int
+	y int
+}
 
-	grid := strings.Split(string(content), "\n")
+func accessible(grid []string) []xy {
 	width := len(grid[0])
 	height := len(grid)
-
-	count := 0
-	papers := 0
+	coords := []xy{}
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			if grid[y][x] == '@' {
 				total := 0
-				papers++
 				for oy := -1; oy <= 1; oy++ {
 					for ox := -1; ox <= 1; ox++ {
 						if oy == 0 && ox == 0 {
@@ -39,12 +34,41 @@ func day_four() {
 				}
 
 				if total < 4 {
-					count++
+					coords = append(coords, xy{x, y})
 				}
 			}
 
 		}
 	}
+	return coords
+}
 
-	fmt.Printf("Day 4 part 1 %d", count) // 1560
+func remove_accessible(grid []string, total int) int {
+	positions := accessible(grid)
+
+	if len(positions) != 0 {
+		for _, position := range positions {
+			row := []byte(grid[position.y])
+			row[position.x] = '.'
+			grid[position.y] = string(row)
+		}
+		total = remove_accessible(grid, total+len(positions))
+	}
+	return total
+}
+
+func day_four() {
+	content, err := os.ReadFile("input/day4.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	grid := strings.Split(string(content), "\n")
+	count := len(accessible(grid))
+
+	total_removed := remove_accessible(grid, 0)
+
+	fmt.Printf("Day 4 part 1 %d\n", count)       // 1560
+	fmt.Printf("Day 4 part 2 %d", total_removed) // 1560
 }
